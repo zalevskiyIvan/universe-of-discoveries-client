d7d405693abfa0f26958fbd7f272ca97e29b5220
zalevskiyIvan <zalevskiyivan342@gmail.com>
2021-02-03 09:26:41 +0300

    fix menu and delete button

--- src/App.js
+++ src/App.js
@@ -1,43 +1,91 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch, useHistory, useLocation } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 import "antd/dist/antd.css";
-import './App.css';
-import StartScreen from './Components/StartScreen/StartScreen';
-import MainPage from './Components/ChoiseSubject/MainPage';
-import PageMenu from './Components/PageMenu/PageMenu';
-import Admins from './Components/Admins/Admins';
-import Menu from './Components/TopMenu/TopMenu';
-import AboutUs from './Components/AboutUs/AboutUs';
-import UserfulLink from './Components/UsefulLinks/UsefulLinks';
-import FullProject from './Components/Project/FullProject';
-import CreateProject from './Components/Project/CreateProject';
-import PostsRender from './Components/PostsRender/PostsRender';
-import Logaut from './Components/Logaut/Logaut';
+import "./App.css";
+import StartScreen from "./Components/StartScreen/StartScreen";
+import MainPage from "./Components/ChoiseSubject/MainPage";
+import PageMenu from "./Components/PageMenu/PageMenu";
+import Admins from "./Components/Admins/Admins";
+import Menu from "./Components/TopMenu/TopMenu";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import UserfulLink from "./Components/UsefulLinks/UsefulLinks";
+import FullProject from "./Components/Project/FullProject";
+import CreateProject from "./Components/Project/CreateProject";
+import PostsRender from "./Components/PostsRender/PostsRender";
+import Logaut from "./Components/Logaut/Logaut";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='App'> 
-        <div className='logaut'>
-          <Logaut/>
+      <div className="App">
+        <div className="logaut">
+          <Logaut />
         </div>
-        
         <Switch>
-          <Route path='/:subject/project/:id' render={() => <FullProject/>}/> 
-          <Route path='/:subject/create-project' render={() => <CreateProject/>}/> 
-          <Route path='/:subject/admins' render={() => <Admins/>}/> 
-          <Route path='/:subject/events' render={() => <div><Menu/><PostsRender type='events' /></div>}/>
-          <Route path='/:subject/project' render={() => <div><Menu/><PostsRender type='projects'/></div>}/>
-          <Route path='/:subject/tasks' render={() => <div><Menu/><PostsRender type='tasks'/></div>}/>
-          <Route path='/:subject/about' render={() => <div><Menu/><AboutUs/></div>}/>
-          <Route path='/:subject/links' render={() => <div><Menu/><UserfulLink/></div>}/>
-          <Route path='/menu' exact  render={() => <MainPage/>}/>
-          <Route path='/:subject' render={() => <PageMenu/>}/>
-          <Route path='/' exact render={() => <StartScreen/>}/>
+          <Route path="/:subject/project/:id" render={() => <FullProject />} />
+          <Route
+            path="/:subject/create-project"
+            render={() => <CreateProject />}
+          />
+          <Route path="/:subject/admins" render={() => <Admins />} />
+          <Route
+            path="/:subject/events"
+            render={() => (
+              <div>
+                <Menu />
+                <PostsRender type="events" />
+              </div>
+            )}
+          />
+          <Route
+            path="/:subject/project"
+            render={() => (
+              <div>
+                <Menu />
+                <PostsRender type="projects" />
+              </div>
+            )}
+          />
+          <Route
+            path="/:subject/tasks"
+            render={() => (
+              <div>
+                <Menu />
+                <PostsRender type="tasks" />
+              </div>
+            )}
+          />
+          <Route
+            path="/:subject/about"
+            render={() => (
+              <div>
+                <Menu />
+                <AboutUs />
+              </div>
+            )}
+          />
+          <Route
+            path="/:subject/links"
+            render={() => (
+              <div>
+                <Menu />
+                <UserfulLink />
+              </div>
+            )}
+          />
+          <Route path="/menu" exact render={() => <MainPage />} />
+          <Route path="/:subject" render={() => <PageMenu />} />
+          <Route path="/" exact render={() => <StartScreen />} />
         </Switch>
-      </div> 
+      </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/Components/TopMenu/TopMenu.tsx
+++ src/Components/TopMenu/TopMenu.tsx
@@ -1,43 +1,75 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom'
-import { getEventT, getShortProjectT, getTaskT } from '../../Reducers/addNewPostReducer'
-import style from './Menu.module.css'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import {
+  getEventT,
+  getShortProjectT,
+  getTaskT,
+} from "../../Reducers/addNewPostReducer";
+import style from "./Menu.module.css";
 const Menu = () => {
-    const history = useHistory()
-    const params:any = useParams()
-    const dispatch = useDispatch()
+  const history = useHistory();
+  const params: any = useParams();
+  const dispatch = useDispatch();
 
-    const events = () => {
-        dispatch(getEventT(1))
-        history.push(`/${params.subject}/events`)
-    }
-    const links = () => {
-        history.push(`/${params.subject}/links`)
-    }
-    const projects = () => {
-        dispatch(getShortProjectT(1))
-        history.push(`/${params.subject}/project`)
-    }
-    const tasks = () => {
-        dispatch(getTaskT(1))
-        history.push(`/${params.subject}/tasks`)
-    }
-    const about = () => {
-        history.push(`/${params.subject}/about`)
-    }
+  const events = () => {
+    dispatch(getEventT(1));
+    history.push(`/${params.subject}/events`);
+  };
+  const links = () => {
+    history.push(`/${params.subject}/links`);
+  };
+  const projects = () => {
+    dispatch(getShortProjectT(1));
+    history.push(`/${params.subject}/project`);
+  };
+  const tasks = () => {
+    dispatch(getTaskT(1));
+    history.push(`/${params.subject}/tasks`);
+  };
+  const about = () => {
+    history.push(`/${params.subject}/about`);
+  };
+  const subject = () => {
+    history.push(`/menu`);
+  };
 
-    return(
-        <div style={{marginTop:100}}>
-            <ul className={style.ul}>
-                <li className={style.li}><span onClick={projects} className={style.a}>Проекты</span></li>
-                <li className={style.li}><span onClick={events} className={style.a}>Мероприятия</span></li>
-                <li className={style.li}><span onClick={tasks} className={style.a}>Задания</span></li>
-                <li className={style.li}><span onClick={about} className={style.a}>О нас</span></li>
-                <li className={style.li}><span onClick={links} className={style.a}>Полезные ссылки</span></li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div style={{ marginTop: 100 }}>
+      <ul className={style.ul}>
+        <li className={style.li}>
+          <span onClick={projects} className={style.a}>
+            Проекты
+          </span>
+        </li>
+        <li className={style.li}>
+          <span onClick={events} className={style.a}>
+            Мероприятия
+          </span>
+        </li>
+        <li className={style.li}>
+          <span onClick={tasks} className={style.a}>
+            Задания
+          </span>
+        </li>
+        <li className={style.li}>
+          <span onClick={about} className={style.a}>
+            О нас
+          </span>
+        </li>
+        <li className={style.li}>
+          <span onClick={links} className={style.a}>
+            Полезные ссылки
+          </span>
+        </li>
+        <li className={style.li}>
+          <span onClick={subject} className={style.a}>
+            выбор предмета
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
